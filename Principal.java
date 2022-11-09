c89a2d1fefddb925d1df2c725245b8ea16ccc723
CamilaRCM <101403356+CamilaRCM@users.noreply.github.com>
2022-11-08 21:15:58 -0300

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.io.IOException;
+
+    public class Principal {
+
+        public class Main {
+            public static void main(String[] args) throws IOException {
+                String path = "./src/br/com/exercicio/ArquivoNomes.txt";
+                NomeCompleto.lerNome(path);
+
+                path = "./src/br/com/exercicio/SobreNomes.txt";
+                NomeCompleto.lerSobreNome(path);
+
+                path = "./src/br/com/exercicio/NomeSobrenome.txt";
+                NomeCompleto.escritor(path);
+                NomeCompleto.lerNomeCompleto(path);
+            }
+
+
+        }
+    }
